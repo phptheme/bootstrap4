9278215f9ddc1089526eb8d2baa0ffcbbc2e237a
phptheme <dev@getphptheme.com>
2019-07-14 07:21:29 +0300

    n/a

--- TableColumn.php
+++ TableColumn.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * @copyright Copyright (c) 2018-2019 PhpTheme Dev Team
+ * @link http://getphptheme.com
+ * @license MIT License
+ */
 namespace PhpTheme\Bootstrap4;
 
 use PhpTheme\Helpers\Html;
