53b07183388d8dc6621f94aa6a5e7f3f46ed44b5
phptheme <dev@getphptheme.com>
2019-07-26 23:45:03 +0300

    n/a

---
+++ Widget.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright Copyright (c) 2018-2019 PhpTheme Dev Team
+ * @link http://getphptheme.com
+ * @license MIT License
+ */
+namespace PhpTheme\Bootstrap4;
+
+abstract class Widget extends \PhpTheme\Core\Widget
+{
+
+    abstract function run();
+
+}
